64a40bff01897368ac443c7264fa921bba84ce72
Thiago Candido <thiagocarvalhocandido8@gmail.com>
2025-08-20 21:59:02 -0300

    initial project structure/testing endpoints with minimal API structure

---
+++ Program.cs
@@ -0,0 +1,76 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.MapGet("/test", () => "Esse é um endpoint de teste");
+
+var produtos = new List<Produto>
+{
+    new Produto { Id = 1, Nome = "Produto A", Preco = 10.0m, Estoque = 100 },
+    new Produto { Id = 2, Nome = "Produto B", Preco = 20.0m, Estoque = 200 }   
+};
+
+app.MapGet("/produtos", () =>
+{
+    return produtos;
+});
+
+app.MapGet("/produtos/{id}", (int id) =>
+{
+    var produto = produtos.FirstOrDefault(p => p.Id == id);
+    return produto == null ? Results.NotFound() : Results.Ok(produto);
+    
+});
+
+
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+class Produto
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public decimal Preco { get; set; }
+    public int Estoque { get; set; }
+}
